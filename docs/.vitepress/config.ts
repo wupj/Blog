0a7aec7398cc750190f3e9d29975403c6a37d2a8
wupj <wdotl@outlook.com>
2023-08-20 16:35:43 +0800

    docs: init
    (cherry picked from commit 7253f0fb9dd704f96df2c5815554fcf336225f75)

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,46 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: '丿一盏孤灯丿的Blog',
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    logo: '/logo.svg',
+    nav: [
+      { text: '笔记', items: [
+        {
+          text: '基础',
+          items: [
+            { text: 'JavaScript', link: 'base/javaScript/index' },
+            { text: 'CSS', link: 'base/css/index' },
+            { text: 'HTML', link: 'base/html/index' }
+          ]
+        },
+        {
+          text: '前端框架',
+          items: [
+            { text: 'Vue', link: 'framework/vue/index' },
+            { text: 'React', link: 'framework/react/index' }
+          ]
+        },
+        {
+          text: '前端可视化',
+          link: '/chart/index'
+        },
+      ]},
+      
+      { text: '面试题', link: '/interview/index' },
+      { text: 'GitHub', link: 'https://github.com/wupj' }
+    ],
+
+    sidebar: [
+      {
+        text: '基础',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' },
+        ],
+      },
+    ]
+  },
+})
---
+++ docs/index.md
@@ -0,0 +1,11 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "丿一盏孤灯丿的Blog"
+  actions:
+    - theme: brand
+      text: 快速开始 →
+      link: /base/javaScript/index
+
